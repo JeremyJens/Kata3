9dd7b64508d49b31a259bcbce0ddd4fc275f8ae7
Usuario <Usuario@DESKTOP-PCHJRGP>
2016-10-21 09:12:03 +0100

    Kata3 Versión 1

---
+++ src/kata/pkg3/Kata3.java
@@ -0,0 +1,11 @@
+package kata.pkg3;
+
+public class Kata3 {
+
+    
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+    
+}
